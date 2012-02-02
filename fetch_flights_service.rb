0013bbf0806c28d662b4ff8cc1f8f14327757c57
Thomas Johan Eggum <thojeggu@gmail.com>
2012-02-02 11:20:47 +0100

    changed

--- fetch_flights_service.rb
+++ fetch_flights_service.rb
@@ -1,18 +1,25 @@
 require 'net/http'
-require 'rexml/document'
+require 'xmlsimple'
+require 'json'
 
 class FetchFlightsService
   
+  @@airport_names = nil
+  @@airline_names = nil
+  
   def fetchXml
-    require 'net/http'
-    require 'rexml/document'
+    if  !@@airport_names || @@airline_names
+      p "fetching airportnames and airline names"
+      @@airportNames = fetchDataFromUrl("http://flydata.avinor.no/airlineNames.asp")
+    end
+ 
+    flights = fetchDataFromUrl("http://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2009-03-10T15:03:00")
+    flights.to_s
+  end
 
-    # Web search for "madonna"
-    url = 'http://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2009-03-10T15:03:00'
-
-    # get the XML data as a string
-    xml_data = Net::HTTP.get_response(URI.parse(url)).body
-
+  def fetchDataFromUrl(url)
+    xml_data = Net::HTTP.get_response(URI.parse(url)).body.to_s
+    data = XmlSimple.xml_in(xml_data)    
   end
 
 end
