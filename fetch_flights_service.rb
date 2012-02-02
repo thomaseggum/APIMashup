a30670097a28df179159700383a915d590b94fc7
Thomas Johan Eggum <thojeggu@gmail.com>
2012-02-02 13:29:50 +0100

    changed

--- fetch_flights_service.rb
+++ fetch_flights_service.rb
@@ -10,12 +10,18 @@ class FetchFlightsService
   def fetchXml
     if  !@@airport_names || !@@airline_names
       p "fetching airportnames and airline names"
-      @@airport_names = fetchDataFromUrl("http://flydata.avinor.no/airportNames.asp")
-      @@airline_names = fetchDataFromUrl("http://flydata.avinor.no/airlineNames.asp")
+      @@airport_names = fetchDataFromUrl("http://flydata.avinor.no/airportNames.asp")["airportName"].to_hash_values {|v| v["code"]}
+      @@airline_names = fetchDataFromUrl("http://flydata.avinor.no/airlineNames.asp")["airlineName"].to_hash_values {|v| v["code"]}
     end
  
-    flights = fetchDataFromUrl("http://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2009-03-10T15:03:00")
-    flights.to_s
+    flights = fetchDataFromUrl("http://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2009-03-10T15:03:00")["flights"][0]["flight"]
+    flights_array = Array.new
+    flights.each do | flight |
+      flight_object = {"city" => @@airport_names[flight["airport"][0]], "time" => Time.parse(flight["schedule_time"][0]).strftime("%H:%M")}
+      flights_array << flight_object
+    end
+    
+    flights_array.to_json
   end
 
   def fetchDataFromUrl(url)
@@ -23,5 +29,13 @@ class FetchFlightsService
     XmlSimple.xml_in(xml_data)
   end
 
-
 end
+
+class Array
+    def to_hash_values(&block)
+      Hash[*self.collect { |v|
+        [block.call(v), v["name"]]
+      }.flatten]
+    end
+end
+
