82ff87f7c96c1ec1018439f8aa0cbdea435d884f
Thomas Johan Eggum <thojeggu@gmail.com>
2012-02-02 11:25:46 +0100

    changed

--- fetch_flights_service.rb
+++ fetch_flights_service.rb
@@ -10,7 +10,8 @@ class FetchFlightsService
   def fetchXml
     if  !@@airport_names || @@airline_names
       p "fetching airportnames and airline names"
-      @@airportNames = fetchDataFromUrl("http://flydata.avinor.no/airlineNames.asp")
+      @@airport_names = fetchDataFromUrl("http://flydata.avinor.no/airportNames.asp")
+      @@airline_names = fetchDataFromUrl("http://flydata.avinor.no/airlineNames.asp")
     end
  
     flights = fetchDataFromUrl("http://flydata.avinor.no/XmlFeed.asp?TimeFrom=1&TimeTo=7&airport=OSL&direction=D&lastUpdate=2009-03-10T15:03:00")
@@ -19,7 +20,6 @@ class FetchFlightsService
 
   def fetchDataFromUrl(url)
     xml_data = Net::HTTP.get_response(URI.parse(url)).body.to_s
-    data = XmlSimple.xml_in(xml_data)    
   end
 
 end
